1f12144ef7ba55ece86d51f5d1765cc521956f42
NgoXCuong <ngocuongcm2005@gmail.com>
2025-12-13 21:19:39 +0700

    update, delete cart

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -1,21 +1,33 @@
-const { BadRequestError } = require("../core/error.response");
-const { Created } = require("../core/success.response");
+const { BadRequestError, NotFoundError } = require("../core/error.response");
+const { Created, OK } = require("../core/success.response");
 const cartModel = require("../models/cart.model");
 const productModel = require("../models/product.model");
+const couponModel = require("../models/coupon.model");
 
-// function calculateFinalPrice(cart, productsData) {
-//   let toalPrice = 0;
-//   for (const product of produtsData) {
-//     let discount = 0;
-//     const product = productsData.find(
-//       (p) => p._id.toString() === product.productId.toString()
-//     );
+async function calculateTotalPrice(findCartUser) {
+  const allProductIds = findCartUser.products.map((p) => p.productId);
 
-//     const discountedPrice =
-//       product.priceProduct * (1 - (product.discountProduct || 0) / 100);
-//     toalPrice += discountedPrice * product.quantity;
-//   }
-// }
+  const productsData = await productModel.find({ _id: { $in: allProductIds } });
+
+  let totalPrice = 0;
+
+  findCartUser.products.forEach((p) => {
+    const product = productsData.find(
+      (prod) => prod._id.toString() === p.productId.toString()
+    );
+    if (product) {
+      const priceAfterDiscount =
+        product.priceProduct -
+        (product.priceProduct * product.discountProduct) / 100;
+      totalPrice += priceAfterDiscount * p.quantity;
+    }
+  });
+
+  findCartUser.totalPrice = totalPrice;
+  await findCartUser.save();
+
+  return totalPrice;
+}
 
 class CartController {
   async createCart(req, res) {
@@ -62,32 +74,171 @@ class CartController {
       await findCartUser.save();
     }
 
-    const allProductIds = findCartUser.products.map(
-      (product) => product.productId
-    );
-
-    const productsData = await productModel.find({
-      _id: { $in: allProductIds },
-    });
-
-    let totalPrice = 0;
-
-    findCartUser.products.forEach((p) => {
-      const product = productsData.find((prod) => {
-        return prod._id.toString() === p.productId.toString();
-      });
-      if (product) {
-        const priceAfterProduct =
-          (product.priceProduct * product.discountProduct) / 100;
-        totalPrice += priceAfterProduct * p.quantity;
-      }
-    });
-
-    findCartUser.totalPrice = totalPrice;
-    await findCartUser.save();
+    await calculateTotalPrice(findCartUser);
 
     return new Created({
       message: "Thêm vào giỏ hàng thành công",
+      metadata: findCartUser,
+    }).send(res);
+  }
+
+  async updateCart(req, res) {
+    const id = req.user;
+    const { productId, newQuantity } = req.body;
+
+    if (!id || !productId) {
+      throw new BadRequestError("Thiếu thông tin cần thiết của giỏ hàng");
+    }
+
+    const findCartUser = await cartModel.findOne({ userId: id });
+    if (!findCartUser) {
+      throw new NotFoundError("Giỏ hàng không tồn tại");
+    }
+
+    const findProductInCart = findCartUser.products.find(
+      (product) => product.productId.toString() === productId
+    );
+
+    if (!findProductInCart) {
+      throw new NotFoundError("Sản phẩm không tồn tại trong giỏ hàng");
+    }
+
+    const productDb = await productModel.findById(productId);
+    if (!productDb) {
+      throw new NotFoundError("Sản phẩm không tồn tại");
+    }
+
+    const currentQuantity = findProductInCart.quantity;
+
+    if (Number(newQuantity) === 0) {
+      productDb.stockProduct += currentQuantity;
+      findCartUser.products = findCartUser.products.filter(
+        (product) => product.productId.toString() !== productId
+      );
+      await productDb.save();
+      await findCartUser.save();
+    } else if (Number(newQuantity) > currentQuantity) {
+      const addQuantity = Number(newQuantity) - currentQuantity;
+
+      if (productDb.stockProduct < addQuantity) {
+        throw new BadRequestError("Số lượng sản phẩm không đủ");
+      }
+
+      findProductInCart.quantity = Number(newQuantity);
+      productDb.stockProduct -= addQuantity;
+      await productDb.save();
+    } else if (Number(newQuantity) < currentQuantity) {
+      const removeQuantity = currentQuantity - Number(newQuantity);
+      findProductInCart.quantity = Number(newQuantity);
+      productDb.stockProduct += removeQuantity;
+      await productDb.save();
+    }
+
+    await calculateTotalPrice(findCartUser);
+
+    return new OK({
+      message: "Cập nhật giỏ hàng thành công",
+      metadata: findCartUser,
+    }).send(res);
+  }
+
+  async deleteProductInCart(req, res) {
+    const id = req.user;
+    const { productId } = req.params;
+
+    if (!id || !productId) {
+      throw new BadRequestError("Thiếu thông tin giỏ hàng");
+    }
+
+    const findCartUser = await cartModel.findOne({ userId: id });
+
+    if (!findCartUser) {
+      throw new NotFoundError("Giỏ hàng không tồn tại");
+    }
+
+    const findProductInCart = findCartUser.products.find(
+      (p) => p.productId.toString() === productId
+    );
+
+    if (!findProductInCart) {
+      throw new NotFoundError("Sản phẩm không tồn tại trong giỏ hàng");
+    }
+
+    const productDb = await productModel.findById(productId);
+    if (!productDb) {
+      throw new NotFoundError("Sản phẩm không tồn tại");
+    }
+
+    findCartUser.products = findCartUser.products.filter(
+      (p) => p.productId.toString() !== productId
+    );
+
+    productDb.stockProduct += findProductInCart.quantity;
+    await productDb.save();
+
+    await calculateTotalPrice(findCartUser);
+
+    return new OK({
+      message: "Xóa sản phẩm khỏi giỏ hàng thành công",
+      metadata: findCartUser,
+    }).send(res);
+  }
+
+  async getCartInUser(req, res) {
+    const id = req.user;
+
+    const findCartUser = await cartModel
+      .findOne({ userId: id })
+      .populate("products.productId");
+
+    const today = new Date();
+
+    const coupons = await couponModel.find({
+      startDate: { $lte: today },
+      endDate: { $gte: today },
+      minPrice: { $lte: findCartUser.totalPrice },
+      quantity: { $gt: 0 },
+    });
+
+    if (!findCartUser) {
+      const newCart = await cartModel.create({
+        userId: id,
+        products: [],
+        coupons: coupons,
+      });
+      return new OK({
+        message: "Lấy giỏ hàng thành công",
+        metadata: { cart: newCart, coupons },
+      }).send(res);
+    }
+
+    return new OK({
+      message: "Lấy giỏ hàng thành công",
+      metadata: { cart: findCartUser, coupons },
+    }).send(res);
+  }
+
+  async updateInfoCart(req, res) {
+    const id = req.user;
+    const { fullName, phoneNumber, address, email } = req.body;
+
+    if (!id || !fullName || !phoneNumber || !address || !email) {
+      throw new BadRequestError("Thiếu thông tin giỏ hàng");
+    }
+
+    const findCartUser = await cartModel.findOne({ userId: id });
+    if (!findCartUser) {
+      throw new NotFoundError("Giỏ hàng không tồn tại");
+    }
+
+    findCartUser.fullName = fullName;
+    findCartUser.phoneNumber = phoneNumber;
+    findCartUser.address = address;
+    findCartUser.email = email;
+    await findCartUser.save();
+
+    return new OK({
+      message: "Cập nhật thông tin giỏ hàng thành công",
       metadata: findCartUser,
     }).send(res);
   }
--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -7,5 +7,17 @@ const { authUser } = require("../middleware/authUser");
 const cartController = require("../controllers/cart.controller");
 
 router.post("/create", authUser, asyncHandler(cartController.createCart));
+router.put("/update", authUser, asyncHandler(cartController.updateCart));
+router.delete(
+  "/delete/:productId",
+  authUser,
+  asyncHandler(cartController.deleteProductInCart)
+);
+router.get("/get", authUser, asyncHandler(cartController.getCartInUser));
+router.put(
+  "/update-info",
+  authUser,
+  asyncHandler(cartController.updateInfoCart)
+);
 
 module.exports = router;
